6895d7bce5ab2eb4749b2564d6b4ffca90b28b9e
Humbetli Mehemmed Cabir <Humbe_li59@itstep.edu.az>
2025-04-17 18:24:38 +0400

    some changes

--- src/pages/carsList/components/CarSearchBar.jsx
+++ src/pages/carsList/components/CarSearchBar.jsx
@@ -54,16 +54,26 @@ const FilterSidebar = () => {
 <div className="bg-white p-4 rounded-lg w-full max-w-xs shadow-md text-sm">
 <h2 className="text-base font-semibold mb-4">Filter</h2>
  
-      {/* Toggle Used / New */}
 <div className="flex mb-4 bg-gray-100 rounded-full p-1">
         {["Used", "New"].map((type) => (
 <button
             key={type}
             onClick={() => {
               setCarType(type);
-              type === "Used"
-                ? updateParams("MinMiles", "0")
-                : updateParams("MaxMiles", "1");
+              if (type === "Used") {
+                setCarType("Used");
+                const params = new URLSearchParams(location.search);
+                params.set("MinMiles", "0");
+                params.delete("MaxMiles"); 
+                navigate({ search: params.toString() });
+              } else {
+                setCarType("New");
+                const params = new URLSearchParams(location.search);
+                params.set("MaxMiles", "1");
+                params.delete("MinMiles"); 
+                navigate({ search: params.toString() });
+              }
+              
             }}
             className={`flex-1 px-4 py-2 rounded-full text-sm font-medium transition ${
               carType === type
